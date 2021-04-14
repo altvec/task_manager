0c75662e25939adc2779a4ee95a838e0cf0f95ef
altvec <me@sergeykalistratov.com>
2021-04-14 22:10:29 +0500

    use inherited factories for Admin, Developer and Manager

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,10 +1,22 @@
 FactoryBot.define do
   factory :user do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    password { "" }
-    email { "MyString" }
-    avatar { "MyString" }
-    type { "" }
+    first_name
+    last_name
+    password
+    email
+    avatar
+    type
+  end
+
+  factory :developer, parent: :user do
+    type { "Developer" }
+  end
+
+  factory :manager, parent: :user do
+    type { "Manager" }
+  end
+
+  factory :admin, parent: :user do
+    type { "Admin" }
   end
 end
