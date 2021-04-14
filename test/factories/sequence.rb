215d5681be462253b66588010e7ce922c8aec441
altvec <me@sergeykalistratov.com>
2021-04-14 22:04:25 +0500

    add factory sequence

---
+++ test/factories/sequence.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :type] do |n|
+    "string#{n}"
+  end
+  sequence(:email) { |n| "user#{n}@example.com" }
+end
