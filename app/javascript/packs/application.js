e23c11a1628e20e430deb410778e2e46b8f6456d
altvec <me@sergeykalistratov.com>
2021-04-30 22:34:07 +0500

    fix imports ordering

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import 'material-design-lite/material.js';
-import TaskBoard from 'components/TaskBoard';
 import WebpackerReact from 'webpacker-react';
+import TaskBoard from 'components/TaskBoard';
 
 WebpackerReact.setup({ TaskBoard });
 
