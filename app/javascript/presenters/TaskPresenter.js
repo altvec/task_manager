390376893d914837a1101299ca85f5beeffa5595
altvec <me@sergeykalistratov.com>
2021-06-14 23:02:37 +0500

    define task states in TaskPrensenter

--- app/javascript/presenters/TaskPresenter.js
+++ app/javascript/presenters/TaskPresenter.js
@@ -1,6 +1,16 @@
 import PropTypes from 'prop-types';
 import PropTypesPresenter from 'utils/PropTypesPresenter';
 
+export const STATES = [
+  { taskState: 'new_task', value: 'New' },
+  { taskState: 'in_development', value: 'In Dev' },
+  { taskState: 'in_qa', value: 'In QA' },
+  { taskState: 'in_code_review', value: 'In CR' },
+  { taskState: 'ready_for_release', value: 'Ready for release' },
+  { taskState: 'released', value: 'Released' },
+  { taskState: 'archived', value: 'Archived' },
+];
+
 export default new PropTypesPresenter(
   {
     id: PropTypes.number,
