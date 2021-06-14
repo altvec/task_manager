f7fe746fb92b210d1ab2b6a449450f903ea83853
altvec <me@sergeykalistratov.com>
2021-06-14 23:07:01 +0500

    add useTasks.js hook

---
+++ app/javascript/hooks/store/useTasks.js
@@ -0,0 +1,17 @@
+import { useSelector } from 'react-redux';
+import { useTasksActions } from 'slices/TasksSlice';
+import { STATES } from 'presenters/TaskPresenter';
+
+const useTasks = () => {
+  const board = useSelector((state) => state.TasksSlice.board);
+  const { loadColumn, loadColumnMore } = useTasksActions();
+  const loadBoard = () => Promise.all(STATES.map(({ taskState }) => loadColumn(taskState)));
+  return {
+    board,
+    loadBoard,
+    loadColumn,
+    loadColumnMore,
+  };
+};
+
+export default useTasks;
